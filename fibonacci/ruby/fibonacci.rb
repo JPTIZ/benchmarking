e231ebfa5d50d387282f60ff25f73e0aab59832a
Joao Paulo Taylor Ienczak Zanette <profissional.jptiz@gmail.com>
2015-08-20 02:04:02 -0300

    Added usage comments

--- fibonacci/ruby/fibonacci.rb
+++ fibonacci/ruby/fibonacci.rb
@@ -1,30 +1,16 @@
-# class Teste
-# 	def initialize(x = 4)
-# 		@x = x
-# 		print "x = #{x}\n"
-# 	end
+#-----------------------------------------------------------------------------
+# Usage:
+# time ruby fibonacci.rb [<flag>] <n>
+# 
+# <flag>:
+#   -r: recursive_fibonacci
+#   -c: cached_fibonacci (recursion + cache)
+#   (blank: loop 3..n if n > 2 || 1)
 #
-# 	def self.x
-# 		return @x
-# 	end
+# <n>:
+#   fibonacci's parameter
 #
-# 	def self.x=(value)
-# 		@x = value
-# 	end
-#
-# 	def x
-# 		return @x
-# 	end
-# end
-#
-# a, t = Teste.new, Teste.new(18)
-# print t.x.to_s+"\n"
-# print a.x.to_s+"\n"
-# print "#{(t.x - a.x)}\n"
-
-#print "\n\n\n"
-#print 666**666**2
-#
+#----------------------------------------------------------------------------
 
 def recursive_fibonacci(n)
     return 1 if n <= 2
@@ -32,10 +18,7 @@ def recursive_fibonacci(n)
 end
 
 def fibonacci(n)
-  return 1 if n <= 2 # poderia ser n == 1 or n == 2, mas assim é mais performático
-                     # e lida melhor com "n"s fora do contexto - mas não altera a
-                     # complexidade de crescimento da função, visto que fazer uma comparação
-                     # tem ordem de crescimento O(1) (ou seja, constante a medida que o dado cresce)
+  return 1 if n <= 2
   fn, fn_0 = 1, 1
   for i in 3..n
     fn, fn_0 = fn + fn_0, fn
@@ -43,9 +26,7 @@ def fibonacci(n)
   return fn
 end
 
-#print "modo (1 = recursivo/qualquer coisa = iterativo): "
 r = ARGV.include?("-r")
-#print "calcular fib de...: "
-x = ARGV[-1].to_i
-puts "#{r ? "r_" : ""}fib(#{x}) = #{r ? recursive_fibonacci(x) : fibonacci(x)}"
+n = ARGV[-1].to_i
+puts "#{r ? "r_" : ""}fib(#{n}) = #{r ? recursive_fibonacci(n) : fibonacci(n)}"
 
